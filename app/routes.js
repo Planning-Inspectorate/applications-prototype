bf0dfca1accd179b22675745c308d072d71bee6e
Phil Sherwin-Nicholls <psherwinnicholls@gmail.com>
2022-01-07 16:44:21 +0000

    move submission into exam stage, link together pages and edit pages

--- app/routes.js
+++ app/routes.js
@@ -5,8 +5,8 @@ require('./routes/errors.js')(router);
 require('./routes/sprint-3.js')(router);
 require('./routes/sprint-4.js')(router);
 require('./routes/sprint-5.js')(router);
-require('./routes/sprint-6.js')(router);
 require('./routes/mvp.js')(router);
+require('./routes/sprint-6.js')(router);
 
   // routes for the type of party /register flow
   router.get("/reset", function (req, res) {
