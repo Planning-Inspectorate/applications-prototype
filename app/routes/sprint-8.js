a2c87f6277bf554be30c30d69d7f701e15167568
sophieottawayuk <65347627+sophieottawayuk@users.noreply.github.com>
2022-06-23 16:57:43 +0100

    Added initial designs for project documents decoupling

--- app/routes/sprint-8.js
+++ app/routes/sprint-8.js
@@ -11,4 +11,16 @@ module.exports = function (router) {
     res.render("sprint-8/examination/representation", representations);
   });
 
+
+  router.post("/decoupling-examination-true", function (req, res) {
+      req.session.data['examinationstage']= "true";
+      res.redirect("june-22-build/decoupling-v1/index");
+  });
+
+  router.post("/decoupling-examination-false", function (req, res) {
+      req.session.data['examinationstage']= "false";
+      res.redirect("june-22-build/decoupling-v1/index");
+  });
+
+
 };
