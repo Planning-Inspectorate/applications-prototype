efe0e6aef3e8aee124927419d9c75e2a27f7bf59
Sophie Ottaway <65347627+sophieottawayuk@users.noreply.github.com>
2023-01-12 11:30:03 +0000

    Added results per page

--- app/routes.js
+++ app/routes.js
@@ -30,6 +30,7 @@ require('./routes/design-sprint-4.js')(router);
 require('./routes/design-sprint-5.js')(router);
 require('./routes/design-sprint-6.js')(router);
 require('./routes/design-sprint-7.js')(router);
+require('./routes/design-sprint-9.js')(router);
 
 
 
