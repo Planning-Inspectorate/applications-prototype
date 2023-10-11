b8329406b0600788959fcee307fa256ceb724f61
chris-bungay <chris.bungay@planninginspectorate.gov.uk>
2023-10-11 09:37:52 +0100

    Updated homepage

--- app/routes.js
+++ app/routes.js
@@ -34,6 +34,7 @@ require('./routes/design-sprint-9.js')(router);
 require('./routes/design-sprint-11.js')(router);
 require('./routes/design-sprint-21.js')(router);
 require('./routes/design-sprint-26.js')(router);
+require('./routes/design-sprint-34.js')(router);
 
 
 
